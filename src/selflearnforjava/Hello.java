bb6b28a99076cfdff0d7e8a7e1ea1306daf11504
n-satoru <n-satoru>
2023-07-02 16:54:24 +0900

    firstcommit

---
+++ src/selflearnforjava/Hello.java
@@ -0,0 +1,13 @@
+package selflearnforjava;
+
+import java.time.LocalDateTime;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		LocalDateTime time = LocalDateTime.now();
+		System.out.println(time);
+		
+		time = null;
+	}
+}
